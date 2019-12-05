4ce12816b8fb4f2676522c5d2d6ed0f01aa79ba7
NarcisWD <cip.ciprian20086@gmail.com>
2019-12-05 21:40:37 +0200

    first

--- GuessNumber/Program.cs
+++ GuessNumber/Program.cs
@@ -10,6 +10,38 @@ namespace GuessNumber
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int numar = random.Next(0, 101);
+            Console.Write("Introduceti un numar intre 0 si 100, inclusiv. Daca ghiciti aveti sansa sa castigati premiul cel mare.\n");
+            string str = Console.ReadLine();
+            int ghicire = int.Parse(str);
+            int limitaSup = 100;
+            int limitaInf = 0;
+            while (ghicire != numar)
+            {
+                if ((ghicire < limitaInf) || (ghicire > limitaSup))
+                {
+                    Console.WriteLine("Numar in afara ariei de ghicire. Introduceti un numar intre " + limitaInf + " si " + limitaSup + ", inclusiv.");
+                    str = Console.ReadLine();
+                    ghicire = int.Parse(str);
+                }
+                if (ghicire < numar)
+                {
+                    limitaInf = ghicire;
+                    Console.WriteLine("Numarul cautat este mai mare. Introduceti un numar intre " + limitaInf + " si " + limitaSup);
+                    str = Console.ReadLine();
+                    ghicire = int.Parse(str);
+                }
+                else
+                {
+                    limitaSup = ghicire;
+                    Console.WriteLine("Numarul cautat este mai mic. Introduceti un numar intre " + limitaInf + " si " + limitaSup);
+                    str = Console.ReadLine();
+                    ghicire = int.Parse(str);
+                }
+            }
+            Console.WriteLine("Felicitari! Ati ghicit corect. Numarul era " + numar + ". Tocmai ati intrat in cursa pentru o Toyota Avensis 2019.");
+            Console.ReadKey();
         }
     }
 }
